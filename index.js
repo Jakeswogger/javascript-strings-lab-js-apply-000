efc04dcb9404f37faa7d0676e313b670e7468b09
Jake Swogger <jakeswogger1@gmail.com>
2019-08-21 00:09:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
+<<<<<<< HEAD
 var topic = "Lets talk about space.";
+=======
+var topic = "space...";
+>>>>>>> fabee72b0983c7fbd59925e5ff04fd1d08fe5432
 
 var conversation = `${topic}`;
